76ded0621b78964a854ada66d5737d046688885a
Gaurav Sablok <gauravcodepro@gmail.com>
2024-12-15 18:34:06 +0100

    applicativesystem
    Finished writing the code and now each read has its own BWT, so taking its time complexity, you dont have visit the read index again, just based on the BWT, you can get the read index. Prepairing for the final release.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod args;
-use ::std::collections::VecDeque;
 use args::BWTArgs;
+use std::cmp::Ord;
 use clap::Parser;
 use std::error::Error;
 use std::fs::File;
@@ -9,28 +9,29 @@ use std::io::{BufRead, BufReader, Write};
 /*
 *Author Gaurav Sablok
 *Universitat Potsdam
-*Date 2024-12-14
+*Date 2024-12-15
 
 rust-bwt-algorithm: The BWT algorithm that has been usually written
 involves the three step, preorder, sort lexically and then take the bwt index.
 I also used this while writing previously the BWT index during my C++ time, but
 not in RUST, i am implementing a new way of using it by declaring a struct based
-implementation and then you can sort the any implementation.
+implementation, so that you can easily, fetch the read based on the BWT, you dont
+have to revisit the index of the read.
 * */
 
 fn main() {
     let args = BWTArgs::parse();
-    let bwt_indices = bwt_deque(&args.bwt_arg).unwrap();
-    println!("The bwt has been written: {}", bwt_indices);
+    let bwt_longread_output = bwt_longread(&args.bwt_arg).unwrap();
+    println!("The bwt has been written: {}", bwt_longread_output);
 }
 
-fn bwt_deque(path: &str) -> Result<String, Box<dyn Error>> {
+fn bwt_longread(path: &str) -> Result<String, Box<dyn Error>> {
     struct ReadSeq {
         header: String,
         sequence: String,
     }
 
-    let mut open_readfile = File::open(path).expect("file not present");
+    let open_readfile = File::open(path).expect("file not present");
     let readfile = BufReader::new(open_readfile);
     let mut bwt_capture_seq: Vec<ReadSeq> = Vec::new();
     let mut bwt_header: Vec<String> = Vec::new();
@@ -52,29 +53,37 @@ fn bwt_deque(path: &str) -> Result<String, Box<dyn Error>> {
 
     #[derive(Debug, Clone, PartialOrd, PartialEq)]
     struct BWTHold {
-        back: String,
-        front: String,
+        id: String,
+        bwt: String,
     }
 
+    let mut final_bwt_write:Vec<BWTHold> = Vec::new();
     for i in bwt_capture_seq.iter() {
         let newstr = String::from(i.sequence.clone());
         let addnew = newstr.chars().map(|x| String::from(x)).collect::<Vec<_>>();
-        let mut final_bwt: Vec<_> = Vec::new();
+        let mut final_bwt:Vec<_> = Vec::new();
+        let mut add_bwt:Vec<_> = Vec::new();
         for i in 0..addnew.len() {
-            let mut map_bwt: Vec<BWTHold> = Vec::new();
+            let mut map_bwt: Vec<_> = Vec::new();
             let front_push = &addnew[0..addnew.len() - i].to_vec().join("");
             let back_push = &addnew[addnew.len() - i..addnew.len()].to_vec().join("");
-            map_bwt.push(BWTHold {
-                back: back_push.to_string(),
-                front: front_push.to_string(),
-            });
-            final_bwt.push(map_bwt);
+            map_bwt.push(back_push);
+            map_bwt.push(front_push);
+            final_bwt.push(format!("{}{}", map_bwt[0], map_bwt[1]));
         }
+        final_bwt.sort();
+        add_bwt.push(final_bwt);
+        final_bwt_write.push(BWTHold{
+          id: i.header.clone(),
+          bwt: add_bwt[0].iter().map(|x| &x[x.len()-1..x.len()]).collect::<Vec<_>>().concat(),
+        })
+
     }
 
-   // implementing a sort step here and then a inverse BWT for the sequence construction.
-   // By implementing the struct based approach, it allows me to index the hash faster and based on
-   // the cyclic rotation, i can easily insert them into a BTreeHashMap.
+    let mut bwt_write_long = File::create("string-bwt-indices.txt").expect("file not present");
+    for i in final_bwt_write.iter(){
+      writeln!(bwt_write_long, "{}\t{}\n", i.id, i.bwt).expect("line not present");
+    }
 
     Ok("BWT has been written".to_string())
 }
